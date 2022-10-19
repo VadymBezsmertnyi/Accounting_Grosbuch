344a69c139650520870035c994f1edb24003a1f2
Bezsmertnyi Vadym <vados199368@gmail.com>
2022-10-19 12:18:50 +0300

    added type

---
+++ src/types/body.ts
@@ -0,0 +1,28 @@
+export type TListOther = {
+  id: number | string;
+  title: string;
+  otherTitle: string[];
+};
+
+export type TSecondTitleList = {
+  title: string;
+  otherTitle: string[];
+};
+
+export type TListDate = {
+  id: number | string;
+  title?: string;
+  imgLogo?: string;
+  text?: string;
+  listOther?: TListOther[];
+  secondTitle?: TSecondTitleList;
+};
+
+export type TDefaultDate = {
+  id: number | string;
+  type: 'other' | 'list';
+  imgMain: string;
+  title?: string;
+  listDate?: TListDate[];
+  listOther?: TListOther[];
+};
