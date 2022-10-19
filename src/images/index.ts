3e4e9dafd2d6748c68bb582aff65d2690d8d5322
Bezsmertnyi Vadym <vados199368@gmail.com>
2022-10-19 12:19:32 +0300

    added images for body

--- src/images/index.ts
+++ src/images/index.ts
@@ -1,2 +1,4 @@
 export { default as LogoTitleIcon } from './Logo';
 export { default as headerImg } from './header.jpg';
+export { default as legalBodyList } from './legalBodyList.png';
+export { default as fopBodyList } from './fopBodyList.png';
