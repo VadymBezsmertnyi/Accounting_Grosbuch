7575faf7e407899ea35eed74fd347f9fb5f327ae
Bezsmertnyi Vadym <vados199368@gmail.com>
2022-10-19 15:58:50 +0300

    create logic items in body

--- src/components/ItemBody/ItemBody.styles.ts
+++ src/components/ItemBody/ItemBody.styles.ts
@@ -1,16 +1,59 @@
 import { createAppTheme } from '../../theme/theme';
-import { headerImg } from '../../images';
 
 const theme = createAppTheme();
 
 export const classes = {
-  itemList: (lastElement: boolean, imgElement: string) => ({
+  itemList: (
+    lastElement: boolean,
+    imgElement: string,
+    type: 'other' | 'list'
+  ) => ({
     background: `url(${imgElement}) center center/cover no-repeat`,
     margin: '200px 0 0 0',
     width: '100%',
     height: '500px',
-    clipPath: `polygon(0 0, 50% 15%, 100% 0, 100% ${
-      lastElement ? '100%' : '85%'
-    }, 50% 100%, 0 ${lastElement ? '100%' : '85%'})`,
+    clipPath:
+      type === 'list'
+        ? `polygon(0 0, 50% 15%, 100% 0, 100% ${
+            lastElement ? '100%' : '85%'
+          }, 50% 100%, 0 ${lastElement ? '100%' : '85%'})`
+        : '',
   }),
+  otherItem: {
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+  },
+  rightOtherItem: (positionElement: 'left' | 'right') => ({
+    width: '100%',
+    height: '20%',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    justifyContent: positionElement === 'left' ? 'flex-end' : 'flex-start',
+  }),
+  imgOtherItem: (positionElement: 'left' | 'right') => ({
+    position: 'relative',
+    [positionElement]: '20%',
+  }),
+  lineOtherItem: {
+    position: 'relative',
+    display: 'flex',
+    width: '60%',
+    bottom: '50%',
+    height: '1px',
+    zIndex: ' -1',
+    background: theme.palette.custom?.main.borderColor || '',
+  },
+  containerTextOtherItem: (positionElement: 'left' | 'right') => ({
+    width: '100%',
+    display: 'flex',
+    justifyContent: positionElement === 'left' ? 'flex-start' : 'flex-end',
+  }),
+  textOtherItem: {
+    width: '50%',
+    padding: '0 8vw',
+  },
 };
--- src/components/ItemBody/ItemBody.tsx
+++ src/components/ItemBody/ItemBody.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Box, Typography } from '@mui/material';
 
-import { classes } from './ItemBody.styles';
 import { TDefaultDate } from '../../types/body';
+import { femaleIcon, maleIcon } from '../../images';
+
+import { classes } from './ItemBody.styles';
 
 interface IItemBodyProps {
   options: TDefaultDate;
@@ -11,8 +13,33 @@ interface IItemBodyProps {
 
 const ItemBody = ({ options, lastElement }: IItemBodyProps) => {
   return (
-    <Box sx={classes.itemList(lastElement, options.imgMain)}>
-      <Typography>TEST BODY</Typography>
+    <Box sx={classes.itemList(lastElement, options.imgMain, options.type)}>
+      {options.type === 'other' && (
+        <Box sx={classes.otherItem}>
+          {options.listDate?.map((option, i) => {
+            const position = (i + 1) % 2 === 0 ? 'right' : 'left';
+            return (
+              <>
+                <Box sx={classes.rightOtherItem(position)}>
+                  {position === 'right' && <Box sx={classes.lineOtherItem} />}
+                  <Box
+                    component={'img'}
+                    src={option.imgLogo}
+                    sx={classes.imgOtherItem(position)}
+                  />
+                  {position === 'left' && <Box sx={classes.lineOtherItem} />}
+                </Box>
+                <Box sx={classes.containerTextOtherItem(position)}>
+                  <Typography sx={classes.textOtherItem}>
+                    {option.text}
+                  </Typography>
+                </Box>
+              </>
+            );
+          })}
+        </Box>
+      )}
+      {options.type === 'list' && <Typography>LIST BODY</Typography>}
     </Box>
   );
 };
