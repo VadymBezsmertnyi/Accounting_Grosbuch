679857b4762b4e2d7c05c88afb248641842a3768
Bezsmertnyi Vadym <vados199368@gmail.com>
2022-10-20 15:00:33 +0300

    added constants feedback

---
+++ src/constants/feedback.ts
@@ -0,0 +1,22 @@
+const DEFAULT_CHECKBOXES_ACTIVITY = {
+  title: 'Вид діяльності',
+  checkboxes: ['Продаж', 'Виробництво', 'Послуги', 'Експорт/імпорт'],
+};
+const DEFAULT_CHECKBOXES_TAXATION_SYSTEM = {
+  title: 'Система оподаткування',
+  checkboxes: [
+    'Загальна система',
+    'Єдиний податок, 2 група',
+    'Єдиний податок, 3 група',
+  ],
+};
+const DEFAULT_CHECKBOXES_OWNERSHIP = {
+  title: 'Форма власності',
+  checkboxes: ['ФОП', 'ТОВ', 'Інша'],
+};
+
+export const DEFAULT_FEEDBACK_CHECKBOXES = {
+  DEFAULT_CHECKBOXES_ACTIVITY,
+  DEFAULT_CHECKBOXES_TAXATION_SYSTEM,
+  DEFAULT_CHECKBOXES_OWNERSHIP,
+};
