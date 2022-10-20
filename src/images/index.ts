082fdf0400a1397b7d16f95f5eb31157b0db0bec
Bezsmertnyi Vadym <vados199368@gmail.com>
2022-10-20 11:47:06 +0300

    added image for feedback

--- src/images/index.ts
+++ src/images/index.ts
@@ -1,5 +1,6 @@
 export { default as LogoTitleIcon } from './Logo';
 export { default as headerImg } from './header.jpg';
+export { default as feedbackImg } from './feedback.jpg';
 export { default as legalBodyList } from './legalBodyList.png';
 export { default as fopBodyList } from './fopBodyList.png';
 
